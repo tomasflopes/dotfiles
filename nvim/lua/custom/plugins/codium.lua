d62d5a36af4fc0f78a2e159b0f50e5a5d1282bd1
Tomás Lopes <1211289@isep.ipp.pt>
2023-12-15 23:40:26 +0000

    added codeium

---
+++ nvim/lua/custom/plugins/codium.lua
@@ -0,0 +1,10 @@
+return {
+  'Exafunction/codeium.vim',
+  event = 'BufEnter',
+  config = function()
+    -- Change '<C-g>' here to any keycode you like.
+    vim.keymap.set('i', '<M-m>', function() return vim.fn['codeium#Accept']() end, { expr = true })
+    vim.keymap.set('i', '<M-]>', function() return vim.fn['codeium#CycleCompletions'](1) end, { expr = true })
+    vim.keymap.set('i', '<M-[>', function() return vim.fn['codeium#CycleCompletions'](-1) end, { expr = true })
+  end
+}
