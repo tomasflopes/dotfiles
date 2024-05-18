3870ed4169f2bcef389f3b6d2a668ab4c2dbdecd
Tomás Lopes <1211289@isep.ipp.pt>
2024-05-18 17:36:10 +0100

    update

--- nvim/lua/custom/plugins/neotree.lua
+++ nvim/lua/custom/plugins/neotree.lua
@@ -168,7 +168,9 @@ return {
             --".null-ls_*",
           },
         },
-        follow_current_file = false, -- This will find and focus the file in the active buffer every
+        follow_current_file = {
+          enabled = false,
+        },
         -- time the current file is changed while the tree is open.
         group_empty_dirs = false, -- when true, empty folders will be grouped together
         hijack_netrw_behavior = 'open_default', -- netrw disabled, opening a directory opens neo-tree
@@ -195,7 +197,9 @@ return {
         },
       },
       buffers = {
-        follow_current_file = true, -- This will find and focus the file in the active buffer every
+        follow_current_file = {
+          enabled = true, -- This will find and focus the file in the active buffer every
+        },
         -- time the current file is changed while the tree is open.
         group_empty_dirs = true, -- when true, empty folders will be grouped together
         show_unloaded = true,
